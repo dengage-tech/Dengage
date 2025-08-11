bad5eb3f2c8e2369ab9bbe7fc5a429f26b97fbfe
Priya Agarwal <priya.agarwal@dengage.com>
2025-08-12 01:12:48 +0530

    Update for version 2.0 with XCFramework build

---
+++ Carthage/Build/Dengage.xcframework/ios-arm64_x86_64-simulator/Dengage.framework/Headers/Dengage.h
@@ -0,0 +1,18 @@
+//
+//  Dengage.h
+//  Dengage
+//
+//  Created by Priya Agarwal on 02/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dengage.
+FOUNDATION_EXPORT double DengageVersionNumber;
+
+//! Project version string for Dengage.
+FOUNDATION_EXPORT const unsigned char DengageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dengage/PublicHeader.h>
+
+
